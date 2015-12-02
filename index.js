9c03d74f50687fa4b4135ad57abdb0c9b825b7fd
Tang,Hejun <hejun.t.tang@intel.com>
2015-12-02 16:20:23 +0800

    save

--- index.js
+++ index.js
@@ -2,16 +2,46 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+
+var allClients = [];
+
+io.on('connection', function(socket) {
+	socket.on('register', function(nickname) {
+		socket.nickname = nickname;
+		allClients.push(socket);
+		console.log( allClients.length );
+		io.emit('online', allClients.length)
+		socket.broadcast.emit('system message', socket.nickname + ' joined');
+	})
+    
+    socket.on('chat message', function(msg) {
+        socket.broadcast.emit('chat message', socket.nickname + ': ' +msg);
+    });
+    socket.on('disconnect', function(msg) {
+    	socket.broadcast.emit('system message', socket.nickname + ' left');
+
+    	var index;
+    	for (var i = allClients.length - 1; i >= 0; i--) {
+    		if(allClients[i].nickname === socket.nickname) {
+    			index = i;
+    		}
+    	}
+    	allClients.splice(index, 1)
+    	io.emit('online', allClients.length)
+    });
+    socket.on('typing', function(msg) {
+    	socket.broadcast.emit('typing');
+    })
+    socket.on('nottyping', function() {
+    	socket.broadcast.emit('nottyping')
+    })
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function() {
+    console.log('listening on *:3000');
 });
+
