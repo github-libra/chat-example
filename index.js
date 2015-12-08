c38d1e7888255ce8fe73194ab2fe781e42e71af9
Tang,Hejun <hejun.t.tang@intel.com>
2015-12-08 16:55:35 +0800

    room and namespace;

--- index.js
+++ index.js
@@ -2,16 +2,42 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+var chatio = io.of('/chat');
+var rooms = [];
+
+chatio.on('connection', function(socket){
+
+	socket.on('subscribe', function(data) {
+		console.log( 'subscribe' );
+		if(data.old) {
+			socket.leave(data.old)
+		}
+		socket.room = data.room;
+		
+		socket.join(data.room);
+		socket.emit('update-room', data.room);
+	})
+  	socket.on('disconnect', function() {
+  		console.log( 'user leave' );
+  	});
+
+  	setInterval(function() {
+  		chatio.in('chatroom1').emit('msg', 'hello chatroom1')
+  	}, 2000);
+
+  	setInterval(function() {
+  		chatio.in('chatroom2').emit('msg', 'hello chatroom2')
+  	}, 2000);
+  	
+
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+
