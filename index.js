9f66c2b6e439cbcbbd1b2d81cf5093c388be9507
Tang,Hejun <hejun.t.tang@intel.com>
2015-12-10 10:27:55 +0800

    add comment;

--- index.js
+++ index.js
@@ -18,23 +18,26 @@ chatio.on('connection', function(socket){
 	socket.on('subscribe', function(data) {
 		console.log( 'subscribe' );
 		if(data.old) {
+			console.log( 'leave' + data.old );
 			socket.leave(data.old)
 		}
+		console.log( socket.rooms );
 		socket.room = data.room;
-		
 		socket.join(data.room);
+		console.log( socket.rooms );
 		socket.emit('update-room', data.room);
 	})
   	socket.on('disconnect', function() {
   		console.log( 'user leave' );
+  		socket.leave(socket.room);
   	});
 
   	setInterval(function() {
-  		chatio.in('chatroom1').emit('msg', 'hello chatroom1')
+  		chatio.in('chatroom1').emit('msg', 'hello chatroom1---' + Math.round(Math.random()*10))
   	}, 2000);
 
   	setInterval(function() {
-  		chatio.in('chatroom2').emit('msg', 'hello chatroom2')
+  		chatio.in('chatroom2').emit('msg', 'hello chatroom2---' + Math.round(Math.random()*10))
   	}, 2000);
   	
 
