d1a17b510715e48411b41b1a2436428a3edf74e3
Tang,Hejun <hejun.t.tang@intel.com>
2015-12-24 15:30:13 +0800

    add room. when client send HTTP request, server cant recognize and will broadcast to every client in the room. when client send websocket request, server will broadcast to every client in the room except the sender.

--- index.js
+++ index.js
@@ -1,46 +1,49 @@
 var app = require('express')();
+var bodyParser = require('body-parser')
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-var chatio = io.of('/chat');
+var chatio = io.of('/ilab');
 var rooms = [];
 
-chatio.on('connection', function(socket){
+chatio.on('connection', function(socket) {
+    // console.log(socket.id);
 
-	socket.on('subscribe', function(data) {
-		console.log( 'subscribe' );
-		if(data.old) {
-			console.log( 'leave' + data.old );
-			socket.leave(data.old)
-		}
-		console.log( socket.rooms );
-		socket.room = data.room;
-		socket.join(data.room);
-		console.log( socket.rooms );
-		socket.emit('update-room', data.room);
-	})
-  	socket.on('disconnect', function() {
-  		console.log( 'user leave' );
-  		socket.leave(socket.room);
-  	});
+    socket.on('subscribe', function(data) {
+        console.log('subscribe to room %s', data.toRoom);
 
-  	setInterval(function() {
-  		chatio.in('chatroom1').emit('msg', 'hello chatroom1---' + Math.round(Math.random()*10))
-  	}, 2000);
+        if (rooms.indexOf(data.toRoom) === -1) {
+            rooms.push(data.toRoom);
+        }
+        socket.join(data.toRoom);
+    });
 
-  	setInterval(function() {
-  		chatio.in('chatroom2').emit('msg', 'hello chatroom2---' + Math.round(Math.random()*10))
-  	}, 2000);
-  	
+    socket.on('unsubscribe', function(data) {
+        console.log('unsubscribe from room %s', data.fromRoom);
+
+        socket.leave(data.fromRoom);
+    })
+
+
+
+    socket.on('delete', function(data) {
+        socket.broadcast.to('env101').emit('update', data)
+    })
 
 });
-
-
+app.put('/env', function(req, res) {
+    console.log(req.body);
+    chatio.to('env101').emit('update', req.body);
+    res.sendStatus(200)
+})
